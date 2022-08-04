8eb7649b94e92ca6a1ef013c22106aa139504711
Ahmad Anvari <ahmad.anv@gmail.com>
2022-08-04 10:40:59 +0430

    Add very simple samples to start

---
+++ cmd/ui.go
@@ -0,0 +1,124 @@
+package cmd
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+	"github.com/spf13/cobra"
+)
+
+var uiCMD = &cobra.Command{
+	Use:   "ui",
+	Short: "Shows the terminal UI",
+	Run:   ui,
+}
+
+var states = []string{"AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+	"HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME",
+	"MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM",
+	"NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX",
+	"UT", "VA", "VT", "WA", "WI", "WV", "WY"}
+
+type Contact struct {
+	firstName   string
+	lastName    string
+	email       string
+	phoneNumber string
+	state       string
+	business    bool
+}
+
+var contacts = make([]Contact, 0)
+
+// Tview
+var pages = tview.NewPages()
+var contactText = tview.NewTextView()
+
+var form = tview.NewForm()
+var contactsList = tview.NewList().ShowSecondaryText(false)
+var flex = tview.NewFlex()
+var text = tview.NewTextView().
+	SetTextColor(tcell.ColorGreen).
+	SetText("(a) to add a new contactq \n(q) to quit")
+
+func ui(cmd *cobra.Command, args []string) {
+	var uiApp = tview.NewApplication()
+
+	contactsList.SetSelectedFunc(func(index int, name string, second_name string, shortcut rune) {
+		setConcatText(&contacts[index])
+	})
+
+	flex.SetDirection(tview.FlexRow).
+		AddItem(tview.NewFlex().
+			AddItem(contactsList, 0, 1, true).
+			AddItem(contactText, 0, 4, false), 0, 6, false).
+		AddItem(text, 0, 1, false)
+
+	flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 113 {
+			uiApp.Stop()
+		} else if event.Rune() == 97 {
+			form.Clear(true)
+			addContactForm()
+			pages.SwitchToPage("Add Contact")
+		}
+		return event
+	})
+
+	pages.AddPage("Menu", flex, true, true)
+	pages.AddPage("Add Contact", form, true, false)
+
+	if err := uiApp.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+
+}
+
+func addContactList() {
+	contactsList.Clear()
+	for index, contact := range contacts {
+		contactsList.AddItem(contact.firstName+" "+contact.lastName, " ", rune(49+index), nil)
+	}
+}
+
+func addContactForm() *tview.Form {
+
+	contact := Contact{}
+
+	form.AddInputField("First Name", "", 20, nil, func(firstName string) {
+		contact.firstName = firstName
+	})
+
+	form.AddInputField("Last Name", "", 20, nil, func(lastName string) {
+		contact.lastName = lastName
+	})
+
+	form.AddInputField("Email", "", 20, nil, func(email string) {
+		contact.email = email
+	})
+
+	form.AddInputField("Phone", "", 20, nil, func(phone string) {
+		contact.phoneNumber = phone
+	})
+
+	form.AddDropDown("State", states, 0, func(state string, index int) {
+		contact.state = state
+	})
+
+	form.AddCheckbox("Business", false, func(business bool) {
+		contact.business = business
+	})
+
+	form.AddButton("Save", func() {
+		contacts = append(contacts, contact)
+		addContactList()
+		pages.SwitchToPage("Menu")
+	})
+
+	return form
+}
+
+func setConcatText(contact *Contact) {
+	contactText.Clear()
+	text := contact.firstName + " " + contact.lastName + "\n" + contact.email + "\n" + contact.phoneNumber
+	contactText.SetText(text)
+}
--- go.mod
+++ go.mod
@@ -3,18 +3,21 @@ module github.com/snapp-incubator/crafting-table
 go 1.18
 
 require (
-	github.com/bxcodec/faker/v3 v3.8.0
+	github.com/gdamore/tcell/v2 v2.5.2
 	github.com/iancoleman/strcase v0.2.0
 	github.com/jmoiron/sqlx v1.3.4
+	github.com/rivo/tview v0.0.0-20220801133142-711ef394f9b3
 	github.com/spf13/cobra v1.3.0
 )
 
 require (
-	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.3.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.7.5 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	golang.org/x/sys v0.0.0-20220318055525-2edf467146b5 // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
