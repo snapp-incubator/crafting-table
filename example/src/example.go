f189b85d91b2b6257d8761dccb8a95c87ef2b09a
Nima Mashhadi <nima.mashhadi@snapp.cab>
2022-08-12 06:37:41 +0430

    add new struct in example file

--- example/src/example.go
+++ example/src/example.go
@@ -6,3 +6,10 @@ type Example struct {
 	Var3 bool   `db:"var3"`
 	Var4 bool   `db:"var4"`
 }
+
+type Example2 struct {
+	Var5 int    `db:"var5"`
+	Var6 string `db:"var6"`
+	Var7 bool   `db:"var7"`
+	Var8 bool   `db:"var8"`
+}
