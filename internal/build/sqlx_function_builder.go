48508a28bec5c2e9ee5ca23353e6fb16ca938615
parsaeissazadeh <parsa.eissazadeh@snapp.cab>
2022-12-17 21:39:36 +0330

    fix a variable name

--- internal/build/sqlx_function_builder.go
+++ internal/build/sqlx_function_builder.go
@@ -505,7 +505,7 @@ if err != nil {
 
 var insertContext *template.Template = template.Must(
 	template.New("insertContext").Parse("query := '{{.Query}}'\n" +
-		`_, err := r.db.NamedExecContext(ctx, query , {{.ObjectName}})
+		`_, err := d.db.NamedExecContext(ctx, query , {{.ObjectName}})
 if err != nil {
 	return err
 }
